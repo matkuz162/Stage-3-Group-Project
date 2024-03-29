45f18e6bf03d50a1cdae27ac9fbf092aa4264287
D4NY41 <dchoudhry2425@gmail.com>
2024-03-29 01:25:28 +0000

    updated

---
+++ broker-manage-product.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="style.css" rel="stylesheet"></link> 
+</head>
+<body>
+    <header class="flex-container">
+
+        <a href="broker-manage-product.html"> <img src="Assets/logo.png" alt="Rose Mortgage"> </a>
+
+        <div>
+            <ul class="nav-links">
+                <li><a href="Broker-product-creation.html">Create Product</a></li>
+                <li><a href="broker-manage-product.html">Manage Product</a></li>
+                <li><a href="broker-account-information.html">Account Details</a></li>
+                <li><a href="LogIn.html">Sign Out</a></li>
+            </ul>
+        </div>
+
+  </header>
+    <main>
+        
+        <div class="largecontainer">
+            <div class="flex-container">
+                <div><h3><b>Manage Products:</b></h3></div>
+        
+                <div class="flex-container">
+                    <div>
+                        <a>Sort By:</a>
+                    </div>
+                    <div>
+                        <select>
+                            <option>Highest Price</option>
+                            <option>Lowest Price</option>
+                            <option>Oldest Product</option>
+                            <option>Newest Product</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="product-layout">
+            <div class="product-header">
+                <h3>Product name</h3>
+                <div>
+                    <a href="broker-product-update.html" class="edit-product-btn">Edit</a>
+                    <a href="#" class="delete-product-btn">Delete</a>
+                </div>
+            </div>
+            <div class="product-section">
+                <div class="manage-product-information">
+                    <div>
+                    
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    <footer>
+    </footer>
+</body>
+</html>
