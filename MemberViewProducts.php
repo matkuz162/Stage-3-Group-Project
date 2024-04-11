fb542c76fb55a4844d4a52ad95d01383d34dd99c
matkuz161 <1panmateuszkuzon@gmail.com>
2024-04-11 14:18:34 +0100

    item ui

--- MemberViewProducts.php
+++ MemberViewProducts.php
@@ -63,7 +63,27 @@ include 'connection.php';
     
 
     <div>
-        <div class="flex-table">11</div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+            </ul>
+            <div class="card-body">
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+
+
+
+
+
         <div class="flex-table">22</div>
         <div class="flex-table">test</div>
         <div class="flex-table">4
