4402425cb48ecd721fe2165072f7fe02da1b4d51
JasmineW2812 <c3021544@my.shu.ac.uk>
2024-04-13 20:03:02 +0100

    broker details

--- broker-account-information.php
+++ broker-account-information.php
@@ -1,11 +1,24 @@
 <?php
+include 'connection.php'; 
+
 session_start();
 
-include 'connection.php';
+if (!isset($_SESSION['Broker_ID'])) {
+  // Redirect to login page
+  header('Location: login.html');
+  exit();
+}
 
+$Broker_ID = $_SESSION['Broker_ID'];
 
+$sql = "SELECT  first_name, last_name, email, phone_number, country, city, postcode, brokage_name, broker_license_number,company_name,company_registration_number,company_country 
+        FROM Broker
+        WHERE Broker_ID='$Broker_ID'";
+$result = $db->query($sql);
+
+foreach ( $result as $row ) {
+}
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -44,35 +57,35 @@ include 'connection.php';
         <form id="editForm" action="update_details.php" method="post">
             <div class="form-group">
                 <label for="first_name" class="form-label">First Name:</label>
-                <input type="text" class="form-control" id="first_name" name="first_name" required readonly>
+                <input type="text" class="form-control" id="first_name" name="first_name"  value="<?php echo $row['first_name']; ?>" required disabled>
             </div>
               <div class="form-group">
                 <label for="last_name" class="form-label">Last Name:</label>
-                <input type="text" class="form-control" id="last_name" name="last_name" required>
+                <input type="text" class="form-control" id="last_name" name="last_name" value="<?php echo $row['last_name']; ?>" required disabled>
               </div>
               <div class="form-group">
                 <label for="email" class="form-label">Email:</label>
-                <input type="email" class="form-control" id="email" name="email" required>
+                <input type="email" class="form-control" id="email" name="email"  value="<?php echo $row['email']; ?>" required disabled>
               </div>
               <div class="form-group">
                 <label for="phone_number" class="form-label">Phone Number:</label>
-                <input type="tel" class="form-control" id="phone_number" name="phone_number" required>
+                <input type="tel" class="form-control" id="phone_number" name="phone_number"  value="<?php echo $row['phone_number']; ?>"required disabled>
               </div>
               <div class="form-group">
                 <label for="country" class="form-label">Country:</label>
-                <input type="text" class="form-control" id="country" name="country" required>
+                <input type="text" class="form-control" id="country" name="country"  value="<?php echo $row['country']; ?>" required disabled>
               </div>
               <div class="form-group">
                 <label for="county" class="form-label">County:</label>
-                <input type="text" class="form-control" id="county" name="county">
+                <input type="text" class="form-control" id="county" name="county" value="<?php echo $row['county']; ?>"disabled> 
               </div>
               <div class="form-group">
                 <label for="city" class="form-label">City:</label>
-                <input type="text" class="form-control" id="city" name="city" required>
+                <input type="text" class="form-control" id="city" name="city" value="<?php echo $row['city']; ?>" required disabled>
               </div>
               <div class="form-group">
                 <label for="postcode" class="form-label">Postcode:</label>
-                <input type="text" class="form-control" id="postcode" name="postcode" required>
+                <input type="text" class="form-control" id="postcode" name="postcode" value="<?php echo $row['postcode']; ?>" required disabled>
               </div>
               <div class="form-group">
                 <label for="password" class="form-label">New Password:</label>
@@ -82,38 +95,37 @@ include 'connection.php';
       
               <div class="form-group">
                 <label for="broker_name" class="form-label">Broker Name:</label>
-                <input type="text" class="form-control" id="broker_name" name="broker_name" required>
+                <input type="text" class="form-control" id="broker_name" name="broker_name" value="<?php echo $row['brokage_name']; ?>" required disabled>
               </div>
               <div class="form-group">
                 <label for="broker_license_number" class="form-label">Broker License Number:</label>
-                <input type="text" class="form-control" id="broker_license_number" name="broker_license_number">
+                <input type="text" class="form-control" id="broker_license_number" name="broker_license_number" value="<?php echo $row['broker_license_number']; ?>" required disabled>
               </div>
               
               <h2 class="mt-5">Company Details:</h2><br>
               
               <div class="form-group">
                 <label for="company_name" class="form-label">Company Name:</label>
-                <input type="text" class="form-control" id="company_name" name="company_name" required>
-              </div>
+                <input type="text" class="form-control" id="company_name" name="company_name" value="<?php echo $row['company_name']; ?>" required disabled>
               <div class="form-group">
                 <label for="company_license_number" class="form-label">Company License Number:</label>
-                <input type="text" class="form-control" id="company_license_number" name="company_license_number">
+                <input type="text" class="form-control" id="company_license_number" name="company_license_number"value=" <?php echo $row['company_registration_number']; ?>" required disabled>
               </div>
               <div class="form-group">
                 <label for="company_country" class="form-label">Country:</label>
-                <input type="text" class="form-control" id="company_country" name="company_country" required>
+                <input type="text" class="form-control" id="company_country" name="company_country" value="<?php echo $row['company_country']; ?>" required disabled>
               </div>
               <div class="form-group">
                 <label for="company_county" class="form-label">County:</label>
-                <input type="text" class="form-control" id="company_county" name="company_county">
+                <input type="text" class="form-control" id="company_county" name="company_county" value="<?php echo $row['']; ?>" required disabled>
               </div>
               <div class="form-group">
                 <label for="company_city" class="form-label">City:</label>
-                <input type="text" class="form-control" id="company_city" name="company_city" required>
+                <input type="text" class="form-control" id="company_city" name="company_city" value="<?php echo $row['']; ?>" required disabled>
               </div>
               <div class="form-group">
                 <label for="company_postcode" class="form-label">Postcode:</label>
-                <input type="text" class="form-control" id="company_postcode" name="company_postcode" required>
+                <input type="text" class="form-control" id="company_postcode" name="company_postcode" value="<?php echo $row['']; ?>" required disabled>
               </div>
               <button type="button" id="editFinancialBtn" class="btn btn-primary btn-lg btn-block" style="width: 100%;">Edit Financial Details</button>
               <button type="submit" id="saveFinancialBtn" class="btn btn-secondary btn-lg btn-block" style="display: none; width: 100%;">Save Financial Details</button>
@@ -140,7 +152,6 @@ include 'connection.php';
     <footer>
     
     </footer>
-
 
 
 
