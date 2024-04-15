079b5199f3d7b4ea9f9d1c0d92919d3b8034080b
D4NY41 <dchoudhry2425@gmail.com>
2024-04-15 23:25:24 +0100

    update

--- broker-account-information.php
+++ broker-account-information.php
@@ -182,6 +182,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     </main>
 
+    <button type="button" class="btn btn-danger btn-lg btn-block mt-4" data-bs-toggle="modal" data-bs-target="#deleteAccountModal">Delete Account</button>
+
+<!-- Modal for Delete Confirmation -->
+<div class="modal fade" id="deleteAccountModal" tabindex="-1" aria-labelledby="deleteAccountModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="deleteAccountModalLabel">Delete Account</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Are you sure you want to delete your account? This action cannot be undone.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <form action="delete-account.php" method="post">
+          <button type="submit" class="btn btn-danger">Delete</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
     <footer>
 
     </footer>
