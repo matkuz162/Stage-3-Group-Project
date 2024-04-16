af191233993d805a29299cd3334a9e0d889b8546
JasmineW2812 <c3021544@my.shu.ac.uk>
2024-04-16 16:06:48 +0100

    reset

--- script.js
+++ script.js
@@ -58,57 +58,60 @@ function calculateMonthlyPayment() {
     document.getElementById("result").innerHTML = "£" + monthlyPayment.toFixed(2);  // result
 }
 
-//reset password captcha
-const captchaTextBox = document.querySelector(".captcha-box");
-const refreshButton = document.querySelector(".refreshButton");
-const captchaInputBox = document.querySelector(".captcha-input");
+// Selecting necessary DOM elements
+const captchaTextBox = document.querySelector(".captch_box input");
+const refreshButton = document.querySelector(".refresh_button");
+const captchaInputBox = document.querySelector(".captch_input input");
 const message = document.querySelector(".message");
 const submitButton = document.querySelector(".button");
 
-//Variable to store generated captcha
-
+// Variable to store generated captcha
 let captchaText = null;
 
-//function to generate captcha
+// Function to generate captcha
 const generateCaptcha = () => {
-    const randomString = math.random().toString(36).subString(2,7);
-    const randomStringArray = randomString.split("");
-    const changeString = randomStringArray.map(char => (Math.random() > 0.5? char.toUpperCase(): char));
-    captchaText = changeString.join("  ");
-    captchaTextBox.value = captchaText;
-    console.log(captchaText);
-
-};
-const refreshBtnClick= () => {
-    generateCaptcha();
-    captchaInputBox.value="";
-    captchaKeyUpValidate();
-};
-const captchaKeyUpValidate = () =>{
- submitButton.classList.toggle("disabled", !captchaInputBox.value);
-
- if(captchaInputBox.value ==="") message.classList.remove("active");
+  const randomString = Math.random().toString(36).substring(2, 7);
+  const randomStringArray = randomString.split("");
+  const changeString = randomStringArray.map((char) => (Math.random() > 0.5 ? char.toUpperCase() : char));
+  captchaText = changeString.join("   ");
+  captchaTextBox.value = captchaText;
+  console.log(captchaText);
 };
 
-const submitBtnClick= ()=>{
-    captchaText = captchaText
+const refreshBtnClick = () => {
+  generateCaptcha();
+  captchaInputBox.value = "";
+  captchaKeyUpValidate();
+};
+
+const captchaKeyUpValidate = () => {
+  //Toggle submit button disable class based on captcha input field.
+  submitButton.classList.toggle("disabled", !captchaInputBox.value);
+
+  if (!captchaInputBox.value) message.classList.remove("active");
+};
+
+// Function to validate the entered captcha
+const submitBtnClick = () => {
+  captchaText = captchaText
     .split("")
     .filter((char) => char !== " ")
     .join("");
-    
-    message.classList.add("active");
-    if(captchaInputBox.value === captchaText){
-        message.innerText ="Entered captcha is correct";
-        message.style.color = "#826afb";
-    }
-    
-    else{
-        message.innerText ="Entered captcha is not correct";
-        message.style.color = "##FF2525";
-    }
+  message.classList.add("active");
+  // Check if the entered captcha text is correct or not
+  if (captchaInputBox.value === captchaText) {
+    message.innerText = "Entered captcha is correct";
+    message.style.color = "#826afb";
+  } else {
+    message.innerText = "Entered captcha is not correct";
+    message.style.color = "#FF2525";
+  }
 };
-//add event listener for the refresh button
-refreshButton.addEventListener("click", refreshBtnClick); 
+
+// Add event listeners for the refresh button, captchaInputBox, submit button
+refreshButton.addEventListener("click", refreshBtnClick);
 captchaInputBox.addEventListener("keyup", captchaKeyUpValidate);
 submitButton.addEventListener("click", submitBtnClick);
+
+// Generate a captcha when the page loads
 generateCaptcha();
