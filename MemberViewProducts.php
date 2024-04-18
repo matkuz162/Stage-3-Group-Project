bb20f064648929d38fc112c14e727923e5fbfa5d
D4NY41 <dchoudhry2425@gmail.com>
2024-04-18 14:52:17 +0100

    update

--- MemberViewProducts.php
+++ MemberViewProducts.php
@@ -41,7 +41,8 @@ $sql = "SELECT *
         LEFT JOIN Quote ON (Quote.Product_ID = Product.Product_ID)
         WHERE Product.expected_income <= (SELECT annual_income FROM financialdetails WHERE RegisteredUser_ID = $RegisteredUser_ID)
         AND Product.expected_outgoings <= (SELECT monthly_spending_amounts FROM financialdetails WHERE RegisteredUser_ID = $RegisteredUser_ID)
-        AND Product.expected_credit_score <= (SELECT credit_score FROM financialdetails WHERE RegisteredUser_ID = $RegisteredUser_ID);";
+        AND Product.expected_credit_score <= (SELECT credit_score FROM financialdetails WHERE RegisteredUser_ID = $RegisteredUser_ID)
+        AND Product.ltv_score <= (SELECT ltv_score FROM financialdetails WHERE RegisteredUser_ID = $RegisteredUser_ID);";
 
 $statement = $db->query($sql);
 
